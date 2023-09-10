285c10e8916c8ba454a5888f90bffa6328198d61
BianchiDev <ifpebianchi@gmail.com>
2023-09-10 16:13:43 -0300

    update CRUD

--- src/main/java/com/test/aprendizado/model/Produto.java
+++ src/main/java/com/test/aprendizado/model/Produto.java
@@ -29,6 +29,65 @@ public class Produto {
         return id;
     }
 
+    public Produto(Integer id, String nome, Integer quantidade, Double valor, String observacao) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+        this.observacao = observacao;
+    }
+
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((quantidade == null) ? 0 : quantidade.hashCode());
+        result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+        result = prime * result + ((observacao == null) ? 0 : observacao.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (quantidade == null) {
+            if (other.quantidade != null)
+                return false;
+        } else if (!quantidade.equals(other.quantidade))
+            return false;
+        if (valor == null) {
+            if (other.valor != null)
+                return false;
+        } else if (!valor.equals(other.valor))
+            return false;
+        if (observacao == null) {
+            if (other.observacao != null)
+                return false;
+        } else if (!observacao.equals(other.observacao))
+            return false;
+        return true;
+    }
+
     public void setId(Integer id) {
         this.id = id;
     }
--- src/main/java/com/test/aprendizado/repository/ProdutoRepository.java
+++ src/main/java/com/test/aprendizado/repository/ProdutoRepository.java
@@ -1,11 +1,13 @@
 package com.test.aprendizado.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 
 import com.test.aprendizado.model.Produto;
 
+import jakarta.persistence.Entity;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
     
 }

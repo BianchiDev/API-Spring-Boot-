d009d09958710c4072c330940261558714a56d60
BianchiDev <ifpebianchi@gmail.com>
2023-09-15 21:07:48 -0300

    Created anotation of the  Security in the swagger

--- src/main/java/com/test/aprendizado/config/OpenApiConfig.java
+++ src/main/java/com/test/aprendizado/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.test.aprendizado.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
@@ -36,6 +39,14 @@ import io.swagger.v3.oas.annotations.servers.Server;
         )
      }
 )
+@SecurityScheme(
+    name = "authBearer",
+    in = SecuritySchemeIn.HEADER,
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    description = "Security desc"
+)
+
 public class OpenApiConfig {
     
 }
